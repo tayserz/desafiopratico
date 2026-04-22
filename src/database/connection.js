add414261d025f4ef30f531d7c4815c25eba1576
tayserz <gabrielsampaio0447@gmail.com>
2026-04-21 22:21:32 -0300

    Atualiza banco Railway

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-    host: 'localhost',
+    host: 'shinkansen.proxy.rlwy.net',
+    port: 42665,
     user: 'root',
-    password: '',
-    database: 'bancodesafio'
+    password: 'XkiKOVjvsOKDIlUWfoSshRfgsuLjndyL',
+    database: 'railway'
 });
 
 module.exports = db;
